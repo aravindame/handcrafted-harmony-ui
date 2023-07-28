1e063249b8267ca5940c6fc87db66e41c582e0a8
aravindame <aravinda.mailbox@gmail.com>
2023-07-28 10:10:49 +0530

    changes added

--- src/components/templates/ProductForm.tsx
+++ src/components/templates/ProductForm.tsx
@@ -97,7 +97,7 @@ const ProductForm: React.FC<IProductFormProps> = ({ onSubmit, product }: IProduc
             ):(<img
               src={imagePreviewUrl}
               alt='Product Preview'
-              style={{ maxWidth: '200px', maxHeight: '200px', marginBottom: '10px' }}
+              style={{ maxWidth: '600px', maxHeight: '800px', marginBottom: '10px' }}
             />)}
             
           </Col>
