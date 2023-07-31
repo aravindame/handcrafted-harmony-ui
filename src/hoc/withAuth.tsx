769b2a0d3cedf8be45150313c5c881f4938b1ff4
aravindame <aravinda.mailbox@gmail.com>
2023-07-31 23:18:53 +0530

    Changes added

--- src/hoc/withAuth.tsx
+++ src/hoc/withAuth.tsx
@@ -1,7 +1,6 @@
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import { useEffect } from 'react';
-import notify from '@/config/toast.config';
 import Spinner from '@/components/atoms/Spinner';
 
 type WithAuthProps = {
